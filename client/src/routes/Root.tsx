3144b54e534e91263dd3e9841a4fc88324b2a69c
toukohan <toukodes@gmail.com>
2023-04-05 22:07:57 +0300

    .

--- client/src/routes/Root.tsx
+++ client/src/routes/Root.tsx
@@ -1,4 +1,4 @@
-import { Outlet, NavLink } from 'react-router-dom'
+import { Outlet } from 'react-router-dom'
 import { getProducts } from '../services/products.service'
 import Copyright from '../components/Copyright'
 import Navigation from '../components/Navigation'
