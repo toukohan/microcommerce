a894fb6f3856e39b06a7de2f48a82d9ccf53ca7c
toukohan <toukodes@gmail.com>
2023-04-05 22:09:44 +0300

    initial event-bus

---
+++ event-bus/index.ts
@@ -0,0 +1,18 @@
+import express from 'express';
+import axios from 'axios';
+
+const app = express();
+
+app.post('/events', (req, res) => {
+    const event = req.body;
+
+    axios.post("http://localhost:4000/events", event);
+    axios.post("http://localhost:4001/events", event);
+    axios.post("http://localhost:4002/events", event);
+
+    res.send({ status: 'OK' });
+});
+
+app.listen(4005, () => {
+    console.log('Listening on 4005');
+});
