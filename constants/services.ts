ec8fbb36e32847b95b9d724ffceff944970a4a70
toukohan <toukodes@gmail.com>
2023-04-07 14:50:18 +0300

    added more services

--- constants/services.ts
+++ constants/services.ts
@@ -1,1 +1,8 @@
-export const eventServiceUrl = 'http://localhost:4005/events';
+export const eventServiceUrl = 'http://localhost:4005';
+
+export const usersServiceUrl = 'http://localhost:4001';
+export const productsServiceUrl = 'http://localhost:4002';
+export const ordersServiceUrl = 'http://localhost:4003';
+export const paymentsServiceUrl = 'http://localhost:4004';
+export const reviewsServiceUrl = 'http://localhost:4006';
+export const moderationServiceUrl = 'http://localhost:4007';
